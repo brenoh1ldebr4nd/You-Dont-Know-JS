135817668bcaf6b37f6ff16e35c8ed21786af251
Breno Hildebrand <breno.h1ldebr4nd@gmail.com>
2021-08-23 19:49:54 -0700

    Solution - Practicing Closure
    The official solution was added too.

--- practicing-closure.js
+++ practicing-closure.js
@@ -16,6 +16,32 @@ function range(start, end) {
     return rangeArray;
 }
 
+/* 
+// Official Solution
+function range(start, end) {
+    start = Number(start) || 0
+
+    if(end === undefined) {
+        return function getEnd(end) {
+            return getRange(start, end)
+        }
+    } 
+    else {
+        end = Number(end) || 0
+        return getRange(start,end)
+    }
+
+    function getRange(start, end) {
+        var ret = []
+        for(let i = start; i <= end; i++) {
+            ret.push(i)
+        }
+        return ret
+    }
+
+}
+*/
+
 range(3,3);    // [3]
 range(3,8);    // [3,4,5,6,7,8]
 range(3,0);    // []
